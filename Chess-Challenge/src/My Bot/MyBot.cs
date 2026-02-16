23da6754585c7813b575f399620013c1c477ab7f
SomeoneOfficial <aprusjeet2011@gmail.com>
2026-02-16 13:11:06 -0600

    hgh

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,31 @@
-﻿using ChessChallenge.API;
+﻿using System.Security.Cryptography.X509Certificates;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
+        Move[] 
+        moves = board.GetLegalMoves();
+        foreach (Move move in moves)
+        {
+            if (move.IsCapture)
+            {
+                return move;
+            }
+        }
         return moves[0];
+
+
+        void checkForLossOfPiece(Board board)
+        {
+            foreach (Move move in board.GetLegalMoves())
+            {
+                if (move.IsCapture)
+                {
+                    return;
+                }
+            }
+        }
     }
 }
